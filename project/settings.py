d318b3b5456352cabdcc3414ce75e5a0f74d6846
navjotsinghpb10 <75848474+navjotsinghpb10@users.noreply.github.com>
2020-12-12 19:36:01 +0530

    heroku

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 
 import os, datetime,sys
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append('/usr/bin/ffmpeg')
@@ -175,3 +176,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
